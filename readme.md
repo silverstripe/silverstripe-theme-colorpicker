5ee8900afdfe6f244a5e9974f11e0f2f1af1b4e8
bergice <andre.kiste@silverstripe.com>
2019-07-10 17:18:09 +1200

    First commit

---
+++ readme.md
@@ -0,0 +1,73 @@
+# Colorpicker Module
+
+[![Build Status](https://travis-ci.org/silverstripe/colorpicker.svg?branch=master)](https://travis-ci.org/silverstripe/colorpicker)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/silverstripe/colorpicker/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/silverstripe/colorpicker/?branch=master)
+[![codecov](https://codecov.io/gh/silverstripe/colorpicker/branch/master/graph/badge.svg)](https://codecov.io/gh/silverstripe/colorpicker)
+[![SilverStripe supported module](https://img.shields.io/badge/silverstripe-supported-0071C4.svg)](https://www.silverstripe.org/software/addons/silverstripe-commercially-supported-module-list/)
+
+This module provides a [Theme color picker](docs/en/01_Features/ThemeColors.md). It allows CMS users to adjust the colors of different areas of their site without requiring developer intervention.
+
+## Installation
+
+To install this module, you can do so with Composer:
+
+```
+composer require silverstripe/colorpicker
+```
+
+## Usage
+
+### Enabling
+
+By default the theme color picker is disabled, to enable this you can adjust your YAML configuration. E.g. in
+`app/_config/config.yml`:
+
+```yml
+SilverStripe\SiteConfig\SiteConfig:
+  enable_theme_color_picker: true
+```
+
+### Adjusting/adding colors
+
+Colorpicker is already supported by the [Watea](https://github.com/silverstripe/cwp-watea-theme) and [Bambusa](https://github.com/silverstripe/bambusa-theme) themes and will work out of the box.
+
+The theme colors are all configurable, so via YAML configuration you can adjust existing colors or add new ones to
+the theme color picker. see `ColorpickerSiteConfigExtension.theme_colors` for a list of the default colors.
+
+```yml
+SilverStripe\SiteConfig\SiteConfig:
+  enable_theme_color_picker: true
+  theme_colors:
+    # Edit existing pink color
+    pink:
+      Color: '#C12099'
+    # Add new brown color
+    brown:
+      Title: 'Brown'
+      CSSClass: 'brown'
+      Color: '#594116'
+```
+
+Now you can add the matching color to your SCSS. Assuming your project is using a custom theme which imports Wātea's
+`main.scss` file, create a `$custom-theme-colors` as follows:
+
+```scss
+// themes/customtheme/scss/main.scss
+
+// Ensure this variable is set before importing watea scss
+$custom-theme-colors: (
+  'pink': #C12099, // Adjusting existing pink color
+  'brown': #594116 // Adding new brown color
+);
+
+@import '../../../watea/src/scss/main';
+```
+
+For more information on using customisable theme colors in the Wātea theme, please refer to the CWP
+Developer documentation: [Using the Wātea theme](https://github.com/silverstripe/cwp/blob/master/docs/en/01_Working_with_projects/14_Using_the_Watea_theme.md).
+
+## Versioning
+
+This library follows [Semver](http://semver.org). According to Semver, you will be able to upgrade to any minor or patch version of this library without any breaking changes to the public API. Semver also requires that we clearly define the public API for this library.
+
+All methods, with `public` visibility, are part of the public API. All other methods are not part of the public API. Where possible, we'll try to keep `protected` methods backwards-compatible in minor/patch versions, but if you're overriding methods then please test your work before upgrading.
---
+++ src/Extensions/ColorpickerSiteConfigExtension.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace SilverStripe\Colorpicker\Extensions;
+
+use SilverStripe\Colorpicker\Forms\ColorPickerField;
+use SilverStripe\Colorpicker\Forms\FontPickerField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+
+/**
+ * Class ColorpickerSiteConfigExtension
+ * @package SilverStripe\Colorpicker\Extensions
+ *
+ * Adds color picker fields to the site config.
+ */
+class ColorpickerSiteConfigExtension extends DataExtension
+{
+    private static $db = array(
+        'HeaderBackground' => 'Varchar(50)',
+        'NavigationBarBackground' => 'Varchar(50)',
+        'CarouselBackground' => 'Varchar(50)',
+        'FooterBackground' => 'Varchar(50)',
+        'AccentColor' => 'Varchar(50)',
+        'TextLinkColor' => 'Varchar(50)',
+    );
+
+    /**
+     * Defines if the theme color picker is enabled in the CMS
+     *
+     * @config
+     * @var boolean
+     */
+    private static $enable_theme_color_picker = false;
+
+    /**
+     * Defines the theme colors that can be selected via the CMS
+     *
+     * @config
+     * @var array
+     */
+    private static $theme_colors = [
+        'default-accent' => [
+            'Title' => 'Default',
+            'CSSClass' => 'default-accent',
+            'Color' => '#0F7EB2',
+        ],
+        'default-background' => [
+            'Title' => 'Default',
+            'CSSClass' => 'default-background',
+            'Color' => '#001F2C',
+        ],
+        'red' => [
+            'Title' => 'Red',
+            'CSSClass' => 'red',
+            'Color' => '#E51016',
+        ],
+        'dark-red' => [
+            'Title' => 'Dark red',
+            'CSSClass' => 'dark-red',
+            'Color' => '#AD161E',
+        ],
+        'pink' => [
+            'Title' => 'Pink',
+            'CSSClass' => 'pink',
+            'Color' => '#B32A95',
+        ],
+        'purple' => [
+            'Title' => 'Purple',
+            'CSSClass' => 'purple',
+            'Color' => '#6239C8',
+        ],
+        'blue' => [
+            'Title' => 'Blue',
+            'CSSClass' => 'blue',
+            'Color' => '#1F6BFE',
+        ],
+        'dark-blue' => [
+            'Title' => 'Dark blue',
+            'CSSClass' => 'dark-blue',
+            'Color' => '#123581',
+        ],
+        'teal' => [
+            'Title' => 'Teal',
+            'CSSClass' => 'teal',
+            'Color' => '#00837A',
+        ],
+        'green' => [
+            'Title' => 'Green',
+            'CSSClass' => 'green',
+            'Color' => '#298436',
+        ],
+        'dark-orange' => [
+            'Title' => 'Dark orange',
+            'CSSClass' => 'dark-orange',
+            'Color' => '#D34300',
+        ],
+        'dark-ochre' => [
+            'Title' => 'Dark ochre',
+            'CSSClass' => 'dark-ochre',
+            'Color' => '#947200',
+        ],
+        'black' => [
+            'Title' => 'Black',
+            'CSSClass' => 'black',
+            'Color' => '#111111',
+        ],
+        'dark-grey' => [
+            'Title' => 'Dark grey',
+            'CSSClass' => 'dark-grey',
+            'Color' => '#555555',
+        ],
+        'light-grey' => [
+            'Title' => 'Light grey',
+            'CSSClass' => 'light-grey',
+            'Color' => '#EAEAEA',
+        ],
+        'white' => [
+            'Title' => 'White',
+            'CSSClass' => 'white',
+            'Color' => '#FFFFFF',
+        ],
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $this->addThemeColorPicker($fields);
+    }
+
+    /**
+     * Add fields for selecting the theme colors for different areas of the site.
+     *
+     * @param  FieldList $fields
+     * @return $this
+     */
+    protected function addThemeColorPicker(FieldList $fields)
+    {
+        // Only show theme color selector if enabled
+        if (!$this->owner->config()->get('enable_theme_color_picker')) {
+            return $this;
+        }
+
+        $fields->addFieldsToTab(
+            'Root.ThemeOptions',
+            [
+                 ColorPickerField::create(
+                     'HeaderBackground',
+                     _t(
+                         __CLASS__ . '.HeaderBackground',
+                         'Header background'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'default-accent',
+                     ])
+                 ),
+                 ColorPickerField::create(
+                     'NavigationBarBackground',
+                     _t(
+                         __CLASS__ . '.NavigationBarBackground',
+                         'Navigation bar background'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'default-accent',
+                     ])
+                 ),
+                 ColorPickerField::create(
+                     'CarouselBackground',
+                     _t(
+                         __CLASS__ . '.CarouselBackground',
+                         'Carousel background'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'default-accent',
+                     ])
+                 )->setDescription(
+                     _t(
+                         __CLASS__ . '.CarouselBackgroundDescription',
+                         'The background color of the carousel when there is no image set.'
+                     )
+                 ),
+                 ColorPickerField::create(
+                     'FooterBackground',
+                     _t(
+                         __CLASS__ . '.FooterBackground',
+                         'Footer background'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'light-grey',
+                         'white',
+                         'default-accent',
+                     ])
+                 ),
+                 ColorPickerField::create(
+                     'AccentColor',
+                     _t(
+                         __CLASS__ . '.AccentColor',
+                         'Accent color'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'light-grey',
+                         'white',
+                         'default-background',
+                     ])
+                 )->setDescription(
+                     _t(
+                         __CLASS__ . '.AccentColorDescription',
+                         'Affects color of buttons, current navigation items, etc. '.
+                         'Please ensure sufficient contrast with background colors.'
+                     )
+                 ),
+                 ColorPickerField::create(
+                     'TextLinkColor',
+                     _t(
+                         __CLASS__ . '.TextLinkColor',
+                         'Text link color'
+                     ),
+                     $this->getThemeOptionsExcluding([
+                         'black',
+                         'light-grey',
+                         'dark-grey',
+                         'white',
+                         'default-background',
+                     ])
+                 ),
+            ]
+        );
+
+        return $this;
+    }
+
+    /**
+     * Returns theme_colors used for ColorPickerField.
+     *
+     * @param  array  $excludedColors list of colors to exclude from the returned options
+     *                                based on the theme color's 'CSSClass' value
+     * @return array
+     */
+    public function getThemeOptionsExcluding($excludedColors = [])
+    {
+        $themeColors = $this->owner->config()->get('theme_colors');
+        $options = [];
+
+        foreach ($themeColors as $themeColor) {
+            if (in_array($themeColor['CSSClass'], $excludedColors)) {
+                continue;
+            }
+
+            $options[] = $themeColor;
+        }
+
+        return $options;
+    }
+
+    /**
+     * If HeaderBackground is not set, assume no theme colors exist and populate some defaults if the color
+     * picker is enabled. We don't use populateDefaults() because we don't want SiteConfig to re-populate its own
+     * defaults.
+     */
+    public function onBeforeWrite()
+    {
+        $colorPickerEnabled = $this->owner->config()->get('enable_theme_color_picker');
+
+        if ($colorPickerEnabled && !$this->owner->HeaderBackground) {
+            $this->owner->update([
+                'HeaderBackground' => 'default-background',
+                'NavigationBarBackground' => 'default-background',
+                'CarouselBackground' => 'default-background',
+                'FooterBackground' => 'default-background',
+                'AccentColor' => 'default-accent',
+                'TextLinkColor' => 'default-accent',
+            ]);
+        }
+    }
+}
